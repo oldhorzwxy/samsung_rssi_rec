fe9f9715abb6993e395e186424da9bb1391d9e7c
horzwxy <wangxiayang.fdt@gmail.com>
2013-08-08 15:31:19 +0800

    first commit

---
+++ src/cn/navior/tool/sumsungrssirec/RecordItem.java
@@ -0,0 +1,50 @@
+package cn.navior.tool.sumsungrssirec;
+
+/**
+ * Java class model for the record of rssi measuring.
+ * @author wangxiayang
+ *
+ */
+public class RecordItem {
+
+	private String mac;
+	private String name;
+	private int rssi;
+	private String datetime;
+	
+	public RecordItem(String mac) {
+		this.mac = mac;
+	}
+	
+	public String getMac() {
+		return mac;
+	}
+	
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRssi() {
+		return rssi;
+	}
+	
+	public void setRssi(int rssi) {
+		this.rssi = rssi;
+	}
+
+	public String getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime( String datetime ) {
+		this.datetime = datetime;
+	}
+}
